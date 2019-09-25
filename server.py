81ec842f750726f5e1044305fcabbba831eca098
guoyunlong16 <guoyl1814@gmail.com>
2019-09-25 11:26:35 +0800

    push txt file contents when establish ws connection
    Change-Id: I0bc5f2437d04a546bdc9d7577b795d3e422e6d54

--- server.py
+++ server.py
@@ -1,14 +1,34 @@
+import time
 from websocket_server import WebsocketServer
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+	#server.send_message_to_all("Hey all, a new client has joined us")
+        short_message = ""
+        middle_message = ""
+        long_message = ""
+        with open("hamlet.txt") as f:
+            short_message=f.read()
+        with open("xiangcunjiaoshi_liucixin.txt") as f:
+            middle_message=f.read()
+        with open("theLongestDayInChangAn.txt") as f:
+           long_message=f.read() 
+        send_message(client, server, short_message)
+        send_message(client, server, middle_message)
+        send_message(client, server, long_message)
 
-
+def send_message(client, server, message):
+        t_end = time.time() + 10
+        count = 1
+        while time.time() < t_end:
+	    server.send_message(client, message)
+            count += 1
+            time.sleep(5)
+    
 # Called for every client disconnecting
 def client_left(client, server):
-	print("Client(%d) disconnected" % client['id'])
+        print("Client(%d) disconnected" % client['id'])
 
 
 # Called when a client sends a message
@@ -18,8 +38,9 @@ def message_received(client, server, message):
 	print("Client(%d) said: %s" % (client['id'], message))
 
 
-PORT=9001
-server = WebsocketServer(PORT)
+PORT=80
+HOST='0.0.0.0'
+server = WebsocketServer(PORT, host=HOST)
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
